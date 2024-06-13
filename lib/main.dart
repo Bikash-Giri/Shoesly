c62bc904207a3f5af9b2edf74f018421cd04f07f
Bikash-Giri <bikashgiri17@gmail.com>
2024-06-13 20:42:13 +0545

    Add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:shoesly/shoes_list/shoes_list_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,41 +29,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final DatabaseReference databaseReference = FirebaseDatabase.instance.ref();
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 2),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const ShoesListPage())));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Firebase Realtime Database Example"),
-      ),
-      body: FutureBuilder<DatabaseEvent>(
-        future: databaseReference.root.once(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text("Error: ${snapshot.error}"));
-          } else {
-            if (snapshot.hasData) {
-              print(snapshot.data!.snapshot.value);
-              Map<dynamic, dynamic> data =
-                  snapshot.data!.snapshot.value as Map<dynamic, dynamic>;
-              return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (context, index) {
-                  String key = data.keys.elementAt(index);
-                  return ListTile(
-                    title: Text(data[key]['name']),
-                    subtitle: Text("Price: ${data[key]['price']}"),
-                  );
-                },
-              );
-            } else {
-              return Center(child: Text("No data available"));
-            }
-          }
-        },
-      ),
-    );
+    return Container(
+        color: Colors.white,
+        child: FlutterLogo(size: MediaQuery.of(context).size.height));
   }
 }
