ea5d16095de72e2d4092185e7a2324b8558e8c6e
Bikash-Giri <bikashgiri17@gmail.com>
2024-06-13 20:42:50 +0545

    Add service class to fetch api

---
+++ lib/shoes_list/shoes_service.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:shoesly/shoes_list/model/shoes_model.dart';
+
+class ShoesService {
+  Future<ShoeModel?> fetchShoesList() async {
+    final DatabaseReference databaseReference = FirebaseDatabase.instance.ref();
+    final data = await databaseReference.root.once();
+    final map = Map<String, dynamic>.from(data.snapshot.value as Map);
+    return ShoeModel.fromMap(map);
+  }
+}
