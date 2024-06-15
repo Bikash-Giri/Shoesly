c487288e47e4bed3f0a0819aa317b46008224900
Bikash-Giri <bikashgiri17@gmail.com>
2024-06-15 16:47:45 +0545

    Add theme bloc

---
+++ lib/resources/theme_bloc.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shoesly/resources/theme.dart'; // ignore: import_of_legacy_library_into_null_safe
+
+class ThemeBloc extends Cubit<ShoeslyTheme> {
+  ThemeBloc({ThemeMode themeMode = ThemeMode.light})
+      : super(ShoeslyTheme(themeMode: themeMode)) {
+    SchedulerBinding.instance.platformDispatcher.onPlatformBrightnessChanged =
+        () {
+      if (state.themeMode == ThemeMode.system) addThemeMode(ThemeMode.system);
+    };
+  }
+
+  void addThemeMode(ThemeMode mode) {
+    emit(ShoeslyTheme(
+      themeMode: mode,
+      statusBarColor: state.statusBarColor,
+      typographySize: state.typographySize,
+    ));
+  }
+
+  void updateLocale(Locale locale) {
+    emit(ShoeslyTheme(
+      themeMode: state.themeMode,
+      statusBarColor: state.statusBarColor,
+      typographySize: state.typographySize,
+    ));
+  }
+
+  ThemeMode themeModeFromRawValue(String? rawThemeMode) {
+    switch (rawThemeMode) {
+      case 'light':
+        return ThemeMode.light;
+      case 'dark':
+        return ThemeMode.dark;
+    }
+    return ThemeMode.system;
+  }
+}
+
+extension ThemeBlocExtension on BuildContext {
+  ShoeslyTheme get shoeslyTheme => BlocProvider.of<ThemeBloc>(this).state;
+
+  ThemeData get theme => shoeslyTheme.themeData;
+
+  TextTheme get textTheme => theme.textTheme;
+
+  ShoeslyThemeColor get color => shoeslyTheme.color;
+}
